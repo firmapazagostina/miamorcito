5bac326260e183b9de28207e9f3883ea0364f1f8
Agostina Firma Paz <114773064+firmapazagostina@users.noreply.github.com>
2024-06-26 13:49:22 -0300

    Delete pag gus directory

--- pag gus/script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.menu');
-const menuDesplegable = document.querySelector('.menu-desplegable')
-menu.addEventListener('click',()=>{
-    menu.classList.toggle("active");
-    menuDesplegable.classList.toggle("click")
-})
